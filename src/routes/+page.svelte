b0298843218aba79a9427a1b1adce837336b0034
Anthony Andrango <aaandrangom@utn.edu.ec>
2025-12-26 22:23:47 -0500

    refactor: remove unused head section and clean up code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  // Sections:
   import About from '@/components/about.svelte';
   import Experience from '@/components/experience.svelte';
   import Projects from '@/components/featuredProjects.svelte';
@@ -10,15 +9,6 @@
 
   let { data }: Props = $props();
 </script>
-
-<svelte:head>
-  <title>Anthony Andrango - aaandrangom</title>
-  <meta property="og:title" content="Anthony Andrango" />
-  <meta property="og:description" content="Turning ideas into reality" />
-  <meta name="image" property="og:image" content="https://res.cloudinary.com/dfg9b8dtx/image/upload/v1766774975/portfolio_f4hvn0.png" />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="630" />
-</svelte:head>
 
 <div class="grid items-center gap-14 py-4 md:py-4">
   <About />
